b1b015b6fc204bd0e1f8f1ce1e9282951299afa2
Jille Timmermans <jille@quis.cx>
2021-04-18 15:39:53 +0100

    Move connectivity stuff in a separate library

--- client/client.go
+++ client/client.go
@@ -4,17 +4,14 @@ import (
 	"context"
 	"flag"
 	"fmt"
-	"io"
 	"log"
 	"strings"
 	"time"
 
-	_ "github.com/Jille/grpc-multi-resolver"
+	"github.com/sgielen/rufs/client/connectivity"
 	"github.com/sgielen/rufs/content"
 	pb "github.com/sgielen/rufs/proto"
 	"github.com/sgielen/rufs/security"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials"
 )
 
 var (
@@ -24,6 +21,13 @@ var (
 	port      = flag.Int("port", 12010, "content server listen port")
 	path      = flag.String("path", "", "root to served content")
 )
+
+func splitMaybeEmpty(str, sep string) []string {
+	if str == "" {
+		return nil
+	}
+	return strings.Split(str, sep)
+}
 
 func main() {
 	flag.Parse()
@@ -38,28 +42,8 @@ func main() {
 		log.Fatalf("Failed to load certificates: %v", err)
 	}
 
-	conn, err := grpc.DialContext(ctx, *discovery, grpc.WithTransportCredentials(credentials.NewTLS(tc)), grpc.WithBlock())
-	if err != nil {
-		log.Fatalf("failed to connect to discovery server: %v", err)
-	}
-	defer conn.Close()
-	c := pb.NewDiscoveryServiceClient(conn)
-
-	endpoints := strings.Split(*flag_endp, ",")
-	if *flag_endp == "" {
-		// Auto-detect endpoints
-		res, err := c.GetMyIP(ctx, &pb.GetMyIPRequest{})
-		if err != nil {
-			log.Fatalf("no ips given and failed to retrieve IP from discovery server, exiting")
-		}
-		endpoints = []string{res.GetIp()}
-	}
-
-	// Add ports to endpoints that don't have any
-	for i := 0; i < len(endpoints); i++ {
-		if !strings.Contains(endpoints[i], ":") {
-			endpoints[i] = fmt.Sprintf("%s:%d", endpoints[i], *port)
-		}
+	if err := connectivity.ConnectToCircle(ctx, *discovery, splitMaybeEmpty(*flag_endp, ","), *port, tc); err != nil {
+		log.Fatalf("Failed to connect to circle %q: %v", *discovery, err)
 	}
 
 	content, err := content.New(fmt.Sprintf(":%d", *port), *path)
@@ -68,50 +52,28 @@ func main() {
 	}
 	go content.Run()
 
-	stream, err := c.Connect(ctx, &pb.ConnectRequest{
-		Endpoints: endpoints,
-	})
-	if err != nil {
-		log.Fatalf("failed to subscribe to discovery server: %v", err)
-	}
-	log.Printf("Connected to RuFS as %s, my endpoints: %s", *username, endpoints)
-
 	for {
-		in, err := stream.Recv()
-		if err == io.EOF {
-			log.Fatalf("discovery server EOF, exiting")
-		}
-		if err != nil {
-			log.Fatalf("discovery server stream error: %v", err)
-		}
-		readdir(ctx, in.Peers, "/")
+		readdir(ctx, "/")
+		time.Sleep(10*time.Second)
 	}
 }
 
-func readdir(ctx context.Context, peers []*pb.Peer, path string) {
+func readdir(ctx context.Context, path string) {
 	ctx, cancel := context.WithTimeout(ctx, 10*time.Second)
 	defer cancel()
 	type peerFile struct {
-		peers []*pb.Peer
+		peers []*connectivity.Peer
 	}
 
 	warnings := []string{}
 	files := make(map[string]*peerFile)
 
-	for _, peer := range peers {
-		conn, err := grpc.DialContext(ctx, "multi:///"+strings.Join(peer.GetEndpoints(), ","), grpc.WithInsecure(), grpc.WithBlock())
-		if err != nil {
-			log.Printf("failed to connect to peer %s, ignoring: %v", peer.GetUsername(), err)
-			continue
-		}
-		defer conn.Close()
-
-		c := pb.NewContentServiceClient(conn)
-		r, err := c.ReadDir(ctx, &pb.ReadDirRequest{
+	for _, peer := range connectivity.AllPeers() {
+		r, err := peer.ContentServiceClient().ReadDir(ctx, &pb.ReadDirRequest{
 			Path: path,
 		})
 		if err != nil {
-			log.Printf("failed to readdir on peer %s, ignoring: %v", peer.GetUsername(), err)
+			log.Printf("failed to readdir on peer %s, ignoring: %v", peer.Name, err)
 			continue
 		}
 		for _, file := range r.Files {
@@ -128,9 +90,9 @@ func readdir(ctx context.Context, peers []*pb.Peer, path string) {
 			peers := ""
 			for _, peer := range file.peers {
 				if peers == "" {
-					peers = peer.GetUsername()
+					peers = peer.Name
 				} else {
-					peers = peers + ", " + peer.GetUsername()
+					peers = peers + ", " + peer.Name
 				}
 			}
 			warnings = append(warnings, fmt.Sprintf("File %s is available on multiple peers (%s), so it was hidden.", filename, peers))
---
+++ client/connectivity/connectivity.go
@@ -0,0 +1,180 @@
+// Package connectivity connects to the discovery server and peers.
+package connectivity
+
+import (
+	"context"
+	"crypto/tls"
+	"fmt"
+	"log"
+	"strings"
+	"sync"
+	"time"
+
+	pb "github.com/sgielen/rufs/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/resolver"
+	"google.golang.org/grpc/resolver/manual"
+)
+
+var (
+	cmtx    sync.Mutex
+	circles []*circle
+)
+
+type circle struct {
+	client      pb.DiscoveryServiceClient
+	myEndpoints []string
+
+	mtx   sync.Mutex
+	peers map[string]*Peer
+}
+
+func ConnectToCircle(ctx context.Context, discoveryServer string, myEndpoints []string, myPort int, tc *tls.Config) error {
+	conn, err := grpc.DialContext(ctx, discoveryServer, grpc.WithTransportCredentials(credentials.NewTLS(tc)), grpc.WithBlock())
+	if err != nil {
+		return fmt.Errorf("failed to connect to discovery server: %v", err)
+	}
+	client := pb.NewDiscoveryServiceClient(conn)
+
+	if len(myEndpoints) == 0 {
+		// Auto-detect endpoints
+		res, err := client.GetMyIP(ctx, &pb.GetMyIPRequest{})
+		if err != nil {
+			return fmt.Errorf("no ips given and failed to retrieve IP from discovery server")
+		}
+		myEndpoints = []string{res.GetIp()}
+	}
+
+	// Add ports to endpoints that don't have any
+	for i, e := range myEndpoints {
+		if !strings.Contains(e, ":") {
+			myEndpoints[i] = fmt.Sprintf("%s:%d", e, myPort)
+		}
+	}
+
+	c := &circle{
+		client:      client,
+		myEndpoints: myEndpoints,
+		peers:       map[string]*Peer{},
+	}
+	go c.run(ctx)
+	circles = append(circles, c)
+	return nil
+}
+
+func (c *circle) run(ctx context.Context) {
+	for {
+		err := c.connect(ctx)
+		if err != nil {
+			log.Printf("Error talking to discovery server: %v")
+			time.Sleep(time.Second)
+		}
+	}
+}
+
+func (c *circle) connect(ctx context.Context) error {
+	stream, err := c.client.Connect(ctx, &pb.ConnectRequest{
+		Endpoints: c.myEndpoints,
+	}, grpc.WaitForReady(true))
+	if err != nil {
+		return fmt.Errorf("failed to subscribe to discovery server: %v", err)
+	}
+	log.Printf("Connected to RuFS. My endpoints: %s", c.myEndpoints)
+
+	for {
+		msg, err := stream.Recv()
+		if err != nil {
+			return fmt.Errorf("discovery server stream error: %v", err)
+		}
+		c.processPeers(ctx, msg.GetPeers())
+	}
+}
+
+func (c *circle) processPeers(ctx context.Context, peers []*pb.Peer) {
+	c.mtx.Lock()
+	defer c.mtx.Unlock()
+	for _, pe := range peers {
+		if po, existing := c.peers[pe.GetUsername()]; existing {
+			po.update(pe)
+		} else {
+			c.peers[pe.GetUsername()] = newPeer(ctx, pe)
+		}
+	}
+}
+
+func newPeer(ctx context.Context, p *pb.Peer) *Peer {
+	r := manual.NewBuilderWithScheme(fmt.Sprintf("rufs-%s-%s", p.GetUsername(), "mycircle"))
+	r.InitialState(peerToResolverState(p))
+	conn, err := grpc.DialContext(ctx, r.Scheme()+":///magic", grpc.WithResolvers(r), grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Failed to dial peer %q: %v", r.Scheme(), err)
+	}
+	return &Peer{
+		Name:     p.GetUsername() + "@mycircle",
+		conn:     conn,
+		resolver: r,
+	}
+}
+
+func peerToResolverState(p *pb.Peer) resolver.State {
+	var s resolver.State
+	for _, e := range p.GetEndpoints() {
+		s.Addresses = append(s.Addresses, resolver.Address{
+			Addr:       e,
+			ServerName: p.GetUsername() + "@mycircle",
+		})
+	}
+	return s
+}
+
+type Peer struct {
+	Name     string
+	conn     *grpc.ClientConn
+	resolver *manual.Resolver
+}
+
+func (p *Peer) update(pe *pb.Peer) {
+	p.resolver.UpdateState(peerToResolverState(pe))
+}
+
+func (p *Peer) ContentServiceClient() pb.ContentServiceClient {
+	return pb.NewContentServiceClient(p.conn)
+}
+
+func (c *circle) GetPeer(name string) *Peer {
+	c.mtx.Lock()
+	defer c.mtx.Unlock()
+	return c.peers[name]
+}
+
+func (c *circle) AllPeers() []*Peer {
+	c.mtx.Lock()
+	defer c.mtx.Unlock()
+	all := make([]*Peer, 0, len(c.peers))
+	for _, p := range c.peers {
+		all = append(all, p)
+	}
+	return all
+}
+
+func GetPeer(name string) *Peer {
+	cmtx.Lock()
+	defer cmtx.Unlock()
+	for _, c := range circles {
+		if p := c.GetPeer(name); p != nil {
+			return p
+		}
+	}
+	return nil
+}
+
+func AllPeers() []*Peer {
+	cmtx.Lock()
+	defer cmtx.Unlock()
+	var all []*Peer
+	for _, c := range circles {
+		all = append(all, c.AllPeers()...)
+	}
+	return all
+}
