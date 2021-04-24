bac9290bd8c85148a70363ef4ed8c6888c5474ab
Jille Timmermans <jille@quis.cx>
2021-04-24 21:10:08 +0100

    transfer: Support for switching to orchestrated mode

--- client/transfer/transfer.go
+++ client/transfer/transfer.go
@@ -12,6 +12,9 @@ import (
 
 	"github.com/sgielen/rufs/client/connectivity"
 	"github.com/sgielen/rufs/client/transfer/cache"
+	"github.com/sgielen/rufs/client/transfer/orchestream"
+	"github.com/sgielen/rufs/client/transfer/passive"
+	"github.com/sgielen/rufs/common"
 	"github.com/sgielen/rufs/intervals"
 	pb "github.com/sgielen/rufs/proto"
 )
@@ -71,14 +74,15 @@ type Transfer struct {
 	peers        []*connectivity.Peer
 	killFetchers func()
 
-	mtx       sync.Mutex
-	serveCond *sync.Cond
-	fetchCond *sync.Cond
-	have      intervals.Intervals
-	want      intervals.Intervals
-	readahead intervals.Intervals
-	pending   intervals.Intervals
-	quit      bool
+	mtx          sync.Mutex
+	serveCond    *sync.Cond
+	fetchCond    *sync.Cond
+	have         intervals.Intervals
+	want         intervals.Intervals
+	readahead    intervals.Intervals
+	pending      intervals.Intervals
+	quitFetchers bool
+	orchestream  *orchestream.Stream
 }
 
 func (t *Transfer) init() {
@@ -109,6 +113,7 @@ func (t *Transfer) Read(ctx context.Context, offset int64, size int64) ([]byte, 
 	if !t.have.Has(offset, offset+size) {
 		t.want.Add(offset, offset+size)
 		t.pending.Add(offset, offset+size)
+		t.byteRangesUpdated()
 		t.fetchCond.Broadcast()
 		for {
 			t.serveCond.Wait()
@@ -137,7 +142,7 @@ func (t *Transfer) simpleFetcher(ctx context.Context) {
 		// We should hold t.mtx at the start of each iteration.
 		var iv intervals.Interval
 		for {
-			if t.quit {
+			if t.quitFetchers {
 				t.mtx.Unlock()
 				return
 			}
@@ -170,44 +175,140 @@ func (t *Transfer) simpleFetcher(ctx context.Context) {
 			res, err := stream.Recv()
 			if err != nil {
 				t.mtx.Lock()
-				if err == io.EOF || t.quit {
+				if err == io.EOF || t.quitFetchers {
 					break
 				}
 				log.Printf("ReadFile(%q).Recv() from %s failed: %v", t.filename, t.peers[pno].Name, err)
 				t.want.Remove(offset, iv.End)
+				t.byteRangesUpdated()
 				t.serveCond.Broadcast()
 				break
 			}
 			if len(res.Data) == 0 {
 				t.mtx.Lock()
+				if res.GetRedirectToOrchestratedDownload() != 0 {
+					if err := t.SwitchToOrchestratedMode(res.GetRedirectToOrchestratedDownload()); err != nil {
+						log.Printf("Failed to switch to orchestrated mode: %v", err)
+						t.want.Remove(offset, iv.End)
+						t.byteRangesUpdated()
+						t.serveCond.Broadcast()
+					}
+				}
 				break
 			}
 			if _, err := t.storage.WriteAt(res.Data, offset); err != nil {
 				t.mtx.Lock()
 				log.Printf("ReadFile(%q): Write to cache failed: %v", t.filename, err)
 				t.want.Remove(offset, iv.End)
+				t.byteRangesUpdated()
 				t.serveCond.Broadcast()
 				break
 			}
-			t.mtx.Lock()
-			t.have.Add(offset, offset+int64(len(res.Data)))
-			t.want.Remove(offset, offset+int64(len(res.Data)))
-			t.serveCond.Broadcast()
-			t.mtx.Unlock()
+			t.receivedBytes(offset, offset+int64(len(res.Data)))
 			offset += int64(len(res.Data))
+			if res.GetRedirectToOrchestratedDownload() != 0 {
+				if err := t.SwitchToOrchestratedMode(res.GetRedirectToOrchestratedDownload()); err != nil {
+					log.Printf("Failed to switch to orchestrated mode (continuing in simple mode): %v", err)
+				}
+			}
 		}
 	}
 }
 
+func (t *Transfer) receivedBytes(start, end int64) {
+	t.mtx.Lock()
+	t.have.Add(start, end)
+	t.want.Remove(start, end)
+	t.byteRangesUpdated()
+	t.serveCond.Broadcast()
+	t.mtx.Unlock()
+}
+
+func (t *Transfer) SwitchToOrchestratedMode(downloadId int64) error {
+	ctx := context.Background()
+	circle := common.CircleFromPeer(t.peers[0].Name)
+	pt := passive.New(ctx, t.storage, downloadId, passiveCallbacks{t})
+	s, err := orchestream.New(ctx, circle, &pb.OrchestrateRequest_StartOrchestrationRequest{
+		DownloadId: downloadId,
+		Filename:   t.filename,
+		Hash:       t.hash,
+	}, pt)
+	if err != nil {
+		return err
+	}
+	t.mtx.Lock()
+	t.orchestream = s
+	t.quitFetchers = true
+	t.killFetchers()
+	t.fetchCond.Broadcast()
+	t.byteRangesUpdated()
+	t.mtx.Unlock()
+	return nil
+}
+
+func (t *Transfer) DownloadId() int64 {
+	return t.orchestream.DownloadId
+}
+
+func (t *Transfer) byteRangesUpdated() {
+	if t.orchestream == nil {
+		return
+	}
+	t.orchestream.SetByteRanges(&pb.OrchestrateRequest_UpdateByteRanges{
+		Have:      intervalsToRanges(t.have),
+		Readnow:   intervalsToRanges(t.want),
+		Readahead: intervalsToRanges(t.readahead),
+	})
+}
+
+func intervalsToRanges(ivs intervals.Intervals) []*pb.Range {
+	l := ivs.Export()
+	ret := make([]*pb.Range, len(l))
+	for i, iv := range l {
+		ret[i] = &pb.Range{
+			Start: iv.Start,
+			End:   iv.End,
+		}
+	}
+	return ret
+}
+
+type passiveCallbacks struct {
+	t *Transfer
+}
+
+func (pc passiveCallbacks) ReceivedBytes(start, end int64) {
+	pc.t.receivedBytes(start, end)
+}
+
+func (pc passiveCallbacks) UploadFailed(peer string) {
+	pc.t.orchestream.UploadFailed(peer)
+}
+
+func (pc passiveCallbacks) SetConnectedPeers(peers []string) {
+	pc.t.orchestream.SetConnectedPeers(peers)
+}
+
+func (t *Transfer) SetHash(hash string) {
+	t.mtx.Lock()
+	t.hash = hash
+	// TODO: Poke orchestream
+	t.mtx.Unlock()
+}
+
 func (t *Transfer) Close() error {
 	t.mtx.Lock()
-	t.quit = true
+	t.quitFetchers = true
 	t.killFetchers()
 	t.want = intervals.Intervals{}
 	t.readahead = intervals.Intervals{}
 	t.pending = intervals.Intervals{}
 	t.fetchCond.Broadcast()
 	t.serveCond.Broadcast()
+	if t.orchestream != nil {
+		t.orchestream.Close()
+	}
+	// TODO: close passive transfers
 	t.mtx.Unlock()
 	return t.storage.Close()
 }
