bcee9a321d2b879b4a73b935ef2749834c3f86ab
Jille Timmermans <jille@quis.cx>
2021-05-02 21:45:26 +0100

    Simplify conflict handling by just calling StartHash()

--- client/shares/conflicts.go
+++ client/shares/conflicts.go
@@ -2,39 +2,10 @@ package shares
 
 import (
 	"context"
-	"errors"
-	"log"
 
 	pb "github.com/sgielen/rufs/proto"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 )
 
 func handleResolveConflictRequest(ctx context.Context, req *pb.ResolveConflictRequest, circle string) {
-	if err := handleResolveConflictRequestImpl(ctx, req, circle); err != nil {
-		if st, ok := status.FromError(err); ok && st.Code() != codes.NotFound {
-			log.Printf("handleResolveConflictRequest(%q) failed: %v", req.GetFilename(), err)
-		}
-	}
-}
-
-func handleResolveConflictRequestImpl(ctx context.Context, req *pb.ResolveConflictRequest, circle string) error {
-	// TODO: Replace this all with a call to StartHash()?
-	localPath, err := resolveRemotePath(circle, req.GetFilename())
-	if err != nil {
-		return err
-	}
-
-	h, err := getFileHashWithStat(localPath)
-	if err == nil && h != "" {
-		// already hashed
-		return nil
-	}
-
-	select {
-	case hashQueue <- hashRequest{localFilename: localPath, remote: req.GetFilename()}:
-		return nil
-	default:
-		return errors.New("hash queue overflow")
-	}
+	StartHash(circle, req.GetFilename())
 }
