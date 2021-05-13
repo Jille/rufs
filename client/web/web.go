325336f6e14f048b45c9631d8363472670542dfb
Jille Timmermans <jille@quis.cx>
2021-05-13 21:54:20 +0100

    Declare more HTTP APIs

--- client/web/web.go
+++ client/web/web.go
@@ -17,6 +17,10 @@ import (
 func Init(port int) {
 	m := http.NewServeMux()
 	m.Handle("/api/config", convreq.Wrap(renderConfig, convreq.WithErrorHandler(errorHandler)))
+	m.Handle("/api/register", convreq.Wrap(registerCircle, convreq.WithErrorHandler(errorHandler)))
+	m.Handle("/api/shares_in_circle", convreq.Wrap(sharesInCircle, convreq.WithErrorHandler(errorHandler)))
+	m.Handle("/api/add_share", convreq.Wrap(addShare, convreq.WithErrorHandler(errorHandler)))
+	m.Handle("/api/open_explorer", convreq.Wrap(openExplorer, convreq.WithErrorHandler(errorHandler)))
 	m.Handle("/", convreq.Wrap(renderStatic))
 	http.HandleFunc("/", authMiddleWare(m))
 	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil); err != nil {
@@ -74,6 +78,39 @@ func renderConfig(ctx context.Context, req *http.Request) convreq.HttpResponse {
 		Circles: config.GetCircles(),
 	}
 	return respondJSON(ret)
+}
+
+type registerCircleGet struct {
+	User   string `schema:"user,required"`
+	Token  string `schema:"token,required"`
+	Circle string `schema:"circle,required"`
+	Ca     string `schema:"ca,required"`
+}
+
+func registerCircle(ctx context.Context, req *http.Request, get registerCircleGet) convreq.HttpResponse {
+	return respond.InternalServerError("not yet implemented")
+}
+
+type sharesInCircleGet struct {
+	Circle string `schema:"circle,required"`
+}
+
+func sharesInCircle(ctx context.Context, req *http.Request, get sharesInCircleGet) convreq.HttpResponse {
+	return respond.InternalServerError("not yet implemented")
+}
+
+type addShareGet struct {
+	Circle string `schema:"circle,required"`
+	Share  string `schema:"share,required"`
+	Local  string `schema:"local,required"`
+}
+
+func addShare(ctx context.Context, req *http.Request, get addShareGet) convreq.HttpResponse {
+	return respond.InternalServerError("not yet implemented")
+}
+
+func openExplorer(ctx context.Context, req *http.Request) convreq.HttpResponse {
+	return respond.InternalServerError("not yet implemented")
 }
 
 func renderStatic(ctx context.Context, req *http.Request) convreq.HttpResponse {
