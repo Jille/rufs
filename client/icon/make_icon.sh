1ec81bca58d5b42c6028cfbb2b1e3786c3185c37
Sjors Gielen <github@sjorsgielen.nl>
2021-05-21 21:39:14 +0200

    The PNG icon works on Windows as well.

--- client/icon/make_icon.sh
+++ client/icon/make_icon.sh
@@ -26,8 +26,6 @@ fi
 
 OUTPUT=icon.go
 echo Generating $OUTPUT
-echo "//+build linux darwin" > $OUTPUT
-echo >> $OUTPUT
 cat "$1" | $GOPATH/bin/2goarray Data icon >> $OUTPUT
 if [ $? -ne 0 ]; then
     echo Failure generating $OUTPUT
